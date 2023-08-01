9d4e2119afec33f822fc2c2cf987dbfeea86e4d7
pytuna <zikzikzikzik16@gmail.com>
2023-08-01 22:14:50 +0700

    Config Logging with Serilog

--- src/BuildingBlocks/Common.Logging/SerilogLogger.cs
+++ src/BuildingBlocks/Common.Logging/SerilogLogger.cs
@@ -1,7 +1,23 @@
-﻿namespace Common.Logging
+﻿using Microsoft.Extensions.Hosting;
+using Serilog;
+namespace Common.Logging
 {
-    public class SerilogLogger
+    public static class SerilogLogger
     {
+        public static Action<HostBuilderContext,LoggerConfiguration> Configure => (context,configuration) =>
+        {
+            var applicationName = context.HostingEnvironment.ApplicationName?
+                .ToLower().Replace(".", "-");
+            var environmentName = context.HostingEnvironment.EnvironmentName ?? "Development";
 
+            configuration
+                .WriteTo.Console()
+                .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}")
+                .Enrich.FromLogContext()
+                .Enrich.WithMachineName()   
+                .Enrich.WithProperty("Application", applicationName)
+                .Enrich.WithProperty("Environment", environmentName)
+                .ReadFrom.Configuration(context.Configuration);
+        };
     }
 }
--- src/Services/Odering.API/Program.cs
+++ src/Services/Odering.API/Program.cs
@@ -1,3 +1,5 @@
+using Serilog;
+using Common.Logging;
 namespace Odering.API
 {
     public class Program
@@ -5,31 +7,43 @@ namespace Odering.API
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            builder.Host.UseSerilog(SerilogLogger.Configure);
+            try
             {
-                app.UseSwagger();
-                app.UseSwaggerUI();
+                Log.Information("Starting Odering.API");
+
+                {
+                    // Add services to the container.
+                    builder.Services.AddControllers();
+                    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+                    builder.Services.AddEndpointsApiExplorer();
+                    builder.Services.AddSwaggerGen();
+                }
+
+                var app = builder.Build();
+                {
+                    // Configure the HTTP request pipeline.
+                    if (app.Environment.IsDevelopment())
+                    {
+                        app.UseSwagger();
+                        app.UseSwaggerUI();
+                    }
+
+                    app.UseHttpsRedirection();
+
+                    app.UseAuthorization();
+
+
+                    app.MapControllers();
+                }
+
+                app.Run();
             }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
+            finally
+            {
+                Log.Fatal("Stopping Odering.API");
+                Log.CloseAndFlush();
+            }
         }
     }
 }
